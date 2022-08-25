e307f19f072e0db0a1fcd9a7943695b712f9d96f
vinay-1729 <vinaymurakala001@gmail.com>
2022-08-25 15:22:59 +0530

    final

--- index.js
+++ index.js
@@ -3,59 +3,36 @@ var b = '';
 var num = [];
 var ans;
 
-// All the numbers and operators input will be stored in an array "num" using function "sendNum()"
 function sendNum(digit){
-
 	num.push(digit);
-
 	if(num.length != 1){
 		a = '';
-		document.getElementById('screen').innerHTML = a;		// clearing the screen.
+		document.getElementById('screen').innerHTML = a;	
 	}
-
-
 	for(i=0; i<num.length ; i++){
-
-		a = a + num[i];				// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
-		
+		a = a + num[i];			
 	}
-
-document.getElementById('screen').innerHTML = a;	// displaying the concatenated string
-
-	
+document.getElementById('screen').innerHTML = a;
 }
-
-// when the user presses "=", function "equalTo()" is called 
 function equalTo(){
 	document.getElementById('screen').innerHTML = '';
-
 	for(i=0; i<num.length ; i++){
-
-		b += num[i];						// concatenating the array "num" into a single string
+		b += num[i];					
 	}
-
 	ans = eval(b);	
-
-	document.getElementById('screen').innerHTML = ans;		// result display
-
+	document.getElementById('screen').innerHTML = ans;
 	while(num.length > 0){
-    	num.pop();				// emptying the array "num"
+    	num.pop();				
 	}
-
 	num.push(ans.toString());
-
-	
 }
 
 
-// When user presses "AC", function "clearScr()" is called
 function clearScr(){
 	document.getElementById('screen').innerHTML = '';
-	
 	while(num.length > 0){
-    	num.pop();				// emptying the array "num"
+    	num.pop();		
 	}
-
 	a ='';	
 	b ='';
 }
